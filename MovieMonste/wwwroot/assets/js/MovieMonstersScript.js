5d1d0e11644d10851dd1ae05c3c1e7614b2f1025
danielevy133 <danielevy133@gmail.com>
2018-08-14 23:10:47 +0300

    last chanege
    last save project

--- MovieMonste/wwwroot/assets/js/MovieMonstersScript.js
+++ MovieMonste/wwwroot/assets/js/MovieMonstersScript.js
@@ -1,9 +1,9 @@
 //write the movie table with movies from the list
 function MovieListToHtml(moviesList) {
-	 var moviesTable = '<div id="bodyContainer" class="container" style="background-color: #ffffff;"><table class="table"><thead><tr><th>Title</th><th>Genere</th><th>UnitsInStock</th><th>ReleaseDate</th><th>Actors</th><th>MinAge</th><th>Language</th><th>UnitPrice</th><th></th></thead><tbody>';
+	var moviesTable = '<div id="bodyContainer" class="container" style="background-color: #ffffff;"><table class="table"><thead><tr><th>Title</th><th>Genere</th><th>UnitsInStock</th><th>ReleaseDate</th><th>Actors</th><th>MinAge</th><th>Language</th><th>UnitPrice</th><th></th></thead><tbody>';
     $.each(moviesList, function (index, movie) {
-        moviesTable = moviesTable.concat('<tr><td>' + movie.Title + '</td>' + '<td>' + movie.Genere + '</td>' + '<td>' + movie.UnitsInStock + '</td>' + '<td>' + movie.ReleaseDate + '</td>' + '<td>' + movie.Actors + '</td>' + '<td>' + movie.MinAge + '</td>' + '<td>' + movie.Language + '</td>' + '<td>' + movie.UnitPrice.Wholesale + '</td></tr>');
-                    })
+                            moviesTable = moviesTable.concat('<tr><td>' + movie.Title + '</td>' + '<td>' + movie.Genere + '</td>' + '<td>' + movie.UnitsInStock + '</td>' + '<td>' + movie.ReleaseDate + '</td>' + '<td>' + movie.Actors + '</td>' + '<td>' + movie.MinAge + '</td>' + '<td>' + movie.Language + '</td>' + '<td>' + movie.UnitPrice.Wholesale + '</td></tr>');
+                       });
         moviesTable = moviesTable.concat('</tbody></table></div>');
     $(bodyContainer).replaceWith(moviesTable);
 }
